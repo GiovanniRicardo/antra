c8e312e114d7d3c9982e6e30faf9efc2ae371681
GiovanniRicardo <60202026+GiovanniRicardo@users.noreply.github.com>
2022-09-26 22:10:22 -0500

    hw34==4

---
+++ hw3/hw4/Script.js
@@ -0,0 +1,34 @@
+const state = {
+    a: "apple",
+    b: "banana",
+    c: "city",
+    d: "dom",
+    e: "end",
+    f: "forEach",
+    g: 'gold'
+};
+    
+for(index in state) {
+const select = document.getElementById("one"); //populate values 
+select.options[select.options.length] = new Option(state[index]);
+}
+
+Object.keys(state).forEach(key => {  //populate property names 
+
+var select = document.getElementById("two");
+select.options[select.options.length] = new Option(key);
+
+})
+
+const droptwo = document.getElementById("two");
+const dropone = document.getElementById("one");
+
+droptwo.addEventListener("change",()=>{
+    dropone.value = state[droptwo.value];
+})
+
+dropone.addEventListener("change",()=>{
+    droptwo.value = Object.keys(state).find(
+        key => state[key] === dropone.value
+      );
+})
